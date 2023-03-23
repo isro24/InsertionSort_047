15660858d03db2ca57e441886212f252df71bb79
isro24 <isro.usman.ft22@mail.umy.ac.id>
2023-03-23 10:54:20 +0700

    membuat procedure input

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -4,3 +4,28 @@ using namespace std;
 int arr[20];											// Membuat Array dengan panjang data 20
 int n;													// Membuat variabel inputan n
 
+void input() {					// Procedure Input
+	while (true)
+	{
+		cout << "Masukkan Jumlah Data pada Array : ";	// Membuat inputan jumlah element Aray
+		cin >> n;										// Membuat varibel inputan n
+
+		if (n <= 20) {									// Membuat kondisi n tidak lebih dari 20
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang and masukkan maksimal 20 Elemen. \n";	// Menampilkan pesan jika data lebih dari 20
+		}
+	}
+	cout << endl;									// Membuat jarak perbaris program
+	cout << "======================" << endl;		// Membuat tampilan susunan data element Array
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl;
+
+	for (int i = 0; i < n; i++)						// Menggunakan perulangan for untuk penyimpanan data pada Array
+	{
+		cout << "Data ke-" << (i + 1) << ": ";		// Masukkan atau menginputkan nilai data n
+		cin >> arr[i];								// Menyimpan nilai data n kedalam Array arr
+	}
+}
